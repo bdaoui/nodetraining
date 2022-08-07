8403d250365c740492039b75474f4734cd094720
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-07 17:59:20 +0200

    added http package loading

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const chalk = import("chalk");
+const chalk = require("chalk");
+
+const http = require('http'); // loading http package
+const port = 3000; // gateway to my code
+
 console.log(chalk.blue("hello world"));
+
+
+const server = http.createServer( (request, response) =>{
+    
+})
