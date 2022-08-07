24c7d26609ee3c3192ebe06cccbe3c36a10a708d
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-07 20:11:55 +0200

    added nodemon

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const chalk = require("chalk");
 
 const http = require('http'); // loading http package
-const port = 3000; // gateway to my code
+const port = 4000; // gateway to my code
 
 console.log(chalk.blue("hello world"));
 
 
 const server = http.createServer( (request, response) =>{
-    if( request.url === '/') return response.write("this is the home page");
+    if(request.url === '/') return response.write("this is the home page");
     if(request.url === '/about') return response.write("this is the about page");
 
-    response.write("<h1>404 page not found <h1>")
+    return response.write("<h1>404 page not found <h1>")
 
 
 })
